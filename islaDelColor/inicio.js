974b7edf0f3055b4a70344ceaedbe890fd2711a5
Clara Jenhee Franco Aguilar <clara20franco@gmail.com>
2026-01-02 18:58:56 -0600

    modo oscuro página principal

--- islaDelColor/inicio.js
+++ islaDelColor/inicio.js
@@ -1,3 +1,39 @@
+//modo oscuro
+const boton = document.getElementById("modoOscuroBtn");
+
+const iconMoon = `
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+     viewBox="0 0 24 24" fill="#d4af37" class="icon-moon">
+  <path d="M12 1.992a10 10 0 1 0 9.236 13.838c.341 -.82 -.476 -1.644 -1.298 -1.31a6.5 6.5 0 0 1 -6.864 -10.787l.077 -.08c.551 -.63 .113 -1.653 -.758 -1.653h-.266l-.068 -.006l-.06 -.002z"/>
+</svg>`;
+
+const iconSun = `
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+     viewBox="0 0 24 24" fill="#d4af37" class="icon-sun">
+  <path d="M12 19a1 1 0 0 1 1 1v2a1 1 0 0 1 -2 0v-2a1 1 0 0 1 1 -1m-4.95 -2.05a1 1 0 0 1 0 1.414l-1.414 1.414a1 1 0 1 1 -1.414 -1.414l1.414 -1.414a1 1 0 0 1 1.414 0m11.314 0l1.414 1.414a1 1 0 0 1 -1.414 1.414l-1.414 -1.414a1 1 0 0 1 1.414 -1.414m-5.049 -9.836a5 5 0 1 1 -2.532 9.674a5 5 0 0 1 2.532 -9.674m-9.315 3.886a1 1 0 0 1 0 2h-2a1 1 0 0 1 0 -2zm18 0a1 1 0 0 1 0 2h-2a1 1 0 0 1 0 -2zm-16.364 -6.778l1.414 1.414a1 1 0 0 1 -1.414 1.414l-1.414 -1.414a1 1 0 0 1 1.414 -1.414m14.142 0a1 1 0 0 1 0 1.414l-1.414 1.414a1 1 0 0 1 -1.414 -1.414l1.414 -1.414a1 1 0 0 1 1.414 0m-7.778 -3.222a1 1 0 0 1 1 1v2a1 1 0 0 1 -2 0v-2a1 1 0 0 1 1 -1"/>
+</svg>`;
+
+boton.addEventListener("click", () => {
+  document.body.classList.toggle("oscuro");
+  const tema = document.body.classList.contains("oscuro") ? "oscuro" : "claro";
+  localStorage.setItem("tema", tema);
+
+  // Cambiar ícono según tema
+  boton.innerHTML = tema === "oscuro" ? iconSun : iconMoon;
+});
+
+// Al cargar la página
+window.addEventListener("DOMContentLoaded", () => {
+  const tema = localStorage.getItem("tema");
+  if (tema === "oscuro") {
+    document.body.classList.add("oscuro");
+    boton.innerHTML = iconSun;
+  } else {
+    boton.innerHTML = iconMoon;
+  }
+});
+
+
 //menú hamburguesa en moviles/celulares
 const toggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('menu-list');
